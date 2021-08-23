0e3b860465ebe4a718efa1312a269ebce20e1e81
Chae-Yeun Park <kaeri17@gmail.com>
2021-08-23 18:43:29 +0200

    print.py only prints numbers.txt

--- print.py
+++ print.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 if __name__ == '__main__':
     p = Path('.')
-    for txt_file in p.glob('./*.txt'):
+    for txt_file in p.glob('./[0-9]*.txt'):
         with txt_file.open() as f:
             print(f.readline())
 
